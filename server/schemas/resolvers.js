57f424212b18c20282a5d781889852c3af54e515
Gunzelb <brandongunzel@gmail.com>
2021-10-19 18:29:38 -0700

    minor changes to connection.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -19,9 +19,13 @@ const resolvers = {
   },
   Mutation: {
     addUser: async (parent, { username, email, password }) => {
-      const user = await User.create({ username, email, password });
-      const token = signToken(user);
-      return { token, user };
+      try {
+        const user = await User.create({ username, email, password });
+        const token = signToken(user);
+        return { token, user };
+      } catch (error) {
+        console.log(error);
+      }
     },
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
